74da3fd2614466940eca3a2ef236d3e74c6f7761
nadeera <nadeera3784@gmail.com>
2018-06-14 21:12:22 -0700

    return get switch database query

--- js/api.js
+++ js/api.js
@@ -61,7 +61,14 @@ api.post('/updateEvents', function (req, res) {
     });
     new mysql_2.MysqlHelpers().close(new mysql_2.MysqlHelpers().initialize());
 });
-  
+
+api.get('/getSwitch', function (req, res) {
+	new mysql_2.MysqlHelpers().getSwitch(new mysql_2.MysqlHelpers().initialize()).then(function (dataset){
+		res.json(dataset);
+	}); 
+	new mysql_2.MysqlHelpers().close(new mysql_2.MysqlHelpers().initialize());
+});
+
 http.createServer(api).listen(api.get('port'), function () {
     console.log("Express server listening on port " + api.get('port'));
 });
